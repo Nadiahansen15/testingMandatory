f62189286712918da55bd9428264660b90452976
Nadia Hansen <nadiahansen.kea@gmail.com>
2021-10-31 12:36:00 +0100

    add minvalidator

--- keaNet/models.py
+++ keaNet/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.forms import ModelForm
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from cellphone.models import cellphone
 
 
 # Create your models here.
 class keaNet(models.Model):
     InternetConnection = models.BooleanField()
-    PhoneLines = models.IntegerField(validators=[MaxValueValidator(8)])
+    PhoneLines = models.IntegerField(validators=[MaxValueValidator(8), MinValueValidator(0)])
     cellphone = models.ForeignKey(cellphone, on_delete=models.CASCADE, default=None)
     totalPrice = models.FloatField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
